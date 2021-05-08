84b92e9a19bc3878e2c710cc202aab9ba027b6cb
Loewan <loewan@ualberta.ca>
2021-05-08 17:50:47 -0600

    add image upload functionality, add picture attribute to image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :user
+  mount_uploader :picture, PictureUploader
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+
 end
